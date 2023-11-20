a3140b5a93bf9c9c83b7c8378672eb870991811a
danny samuel <adheichsn01@gmail.com>
2023-11-20 12:06:12 +0700

    fix bugs

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,15 +1,26 @@
-'use client'
-import React from 'react'
-import { Button } from './ui/button'
-import { signOut } from 'next-auth/react'
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { Button } from './ui/button';
+import { signOut } from 'next-auth/react';
 
 const UserAccountNav = () => {
-  return (
-    <Button onClick={() => signOut({
-        redirect: true,
-        callbackUrl: `${window.location.origin}/sign-in`
-    })} variant='destructive'>Sign Out</Button>
-  )
-}
+  const router = useRouter();
+  // console.log('window.location.origin:', window.location.origin);
 
-export default UserAccountNav
+  return (
+    <Button
+      onClick={() =>
+        signOut({
+          redirect: true,
+          callbackUrl: `${window.location.origin}/sign-in`,
+        }).then(() => router.push('/sign-in'))
+      }
+      variant='destructive'
+    >
+      Sign Out
+    </Button>
+  );
+};
+
+export default UserAccountNav;
